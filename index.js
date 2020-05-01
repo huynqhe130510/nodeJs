c4be300dc55daecfd08da72f6288d1ff5b26e40c
= <=>
2020-05-01 11:07:41 +0700

    query parameters

--- index.js
+++ index.js
@@ -5,18 +5,31 @@ var port = 3000;
 app.set('view engine', 'pug')
 app.set('views', './views')
 
+var users = [
+    { id: 1, name: 'Huy' },
+    { id: 2, name: 'Hieu' }
+]
+
 app.get('/', function(req, res) {
     res.render('index', {
         name: 'a'
     });
 });
 
-app.get('/user', function(req, res) {
+app.get('/users', function(req, res) {
     res.render('users/index', {
-        users: [
-            { id: 1, name: 'Huy' },
-            { id: 2, name: 'Hieu' }
-        ]
+        users: users
+    });
+});
+
+app.get('/users/search', function(req, res) {
+    var q = req.query.q;
+    var matchedUser = users.filter(function(user) {
+        return user.name.toLowerCase().indexOf(q.toLocaleLowerCase()) !== -1;
+    });
+
+    res.render('users/index', {
+        users: matchedUser
     });
 });
 
