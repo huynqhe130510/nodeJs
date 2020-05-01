335bb12fd9f72ef311198d6d99e4ae30e1aabcdb
= <=>
2020-05-01 21:43:40 +0700

    static file in express

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ var userRoutes = require('./routes/user.routes');
 var port = 3000;
 
 var app = express();
-app.set('view engine', 'pug')
-app.set('views', './views')
+app.set('view engine', 'pug');
+app.set('views', './views');
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
 
 app.get('/', function(req, res) {
     res.render('index', {
